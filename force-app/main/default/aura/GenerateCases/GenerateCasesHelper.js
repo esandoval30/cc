ce9cc8be03ccac39718505416c6d62f75f2dc6a7
esandoval30 <esandoval@salesforce.com>
2020-09-24 17:42:43 +0100

    Closed ListView, Fixing GCHelper.js, rename case update quick action

--- force-app/main/default/aura/GenerateCases/GenerateCasesHelper.js
+++ force-app/main/default/aura/GenerateCases/GenerateCasesHelper.js
@@ -1,6 +1,4 @@
-({
-	
-    
+({    
     showToast : function(component, event, helper) {
         var toastEvent = $A.get("e.force:showToast");
         toastEvent.setParams({
@@ -8,20 +6,14 @@
             "message": "Cases are being generated. Check the New Cases List view"
         });
         toastEvent.fire();
-	}
-})
-
-({    
+	}, 
     navigateToCaseObject : function (component, event, helper) {
     	 var homeEvent = $A.get("e.force:navigateToObjectHome");
 		    homeEvent.setParams({
         		"scope": "Case"
     	});
     	homeEvent.fire();
-	}
-})
-
-({
+	},
     closeQuickAction : function (component, event, helper) {
         var dismissActionPanel = $A.get("e.force:closeQuickAction");
         dismissActionPanel.fire();
