a2e6ddd7f2596d7cf5828e6d61f82c545ba1d46d
esandoval30 <esandoval@salesforce.com>
2021-04-07 12:10:25 +0100

    Install ECC Value Analytics on scratch org

--- orgInit.sh
+++ orgInit.sh
@@ -6,8 +6,8 @@ sfdx force:user:password:generate
 sfdx force:user:permset:assign --permsetname Audit_Fields
 sfdx force:user:permset:assign --permsetname EinsteinAgent
 
-#install Einstein Language Intent Accuracy Package
-sfdx force:package:install -p 04t4J000002AU7A --noprompt
+#install Einstein Case Classification Value Analytics
+sfdx force:package:install -p 04tB0000000UQjfIAG --noprompt
 
 #bulk load Closed Cases
 sfdx shane:data:dates:update -r 09-24-2020
